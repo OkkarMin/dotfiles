8463c1a10a3bf1340c88137911b453d1ca10ae85
OkkarMin <omin001@e.ntu.edu.sg>
2023-12-15 12:04:25 +0800

    fish: press tab to accept autosuggestion

--- private_dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
-  fish_vi_key_bindings
-  bind -M insert -m default jj backward-char force-repaint
+    fish_vi_key_bindings
+    bind -M insert -m default jj backward-char force-repaint
+    bind -M insert -m default \t accept-autosuggestion
 end
 fzf_key_bindings
