c2f69e7d45a30333a972384f43fb5cd50d952e8d
OkkarMin <omin001@e.ntu.edu.sg>
2023-12-20 07:55:36 +0800

    fish: remove tab as auto suggestion

--- private_dot_config/private_fish/functions/fish_user_key_bindings.fish
+++ private_dot_config/private_fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
     fish_vi_key_bindings
-    bind -M insert -m default jj backward-char force-repaint
-    bind -M insert -m default \t accept-autosuggestion
+    fzf_key_bindings
+
+    bind --mode insert --sets-mode default jj repaint
 end
-fzf_key_bindings
