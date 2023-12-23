286fec7bbf8176660d4479fae75d24df8fa295ad
OkkarMin <omin001@e.ntu.edu.sg>
2023-12-23 09:33:23 +0800

    nvim: better helm support

--- private_dot_config/nvim/lua/plugins/vim-helm.lua
+++ private_dot_config/nvim/lua/plugins/vim-helm.lua
@@ -1,5 +1,22 @@
 return {
   {
     "towolf/vim-helm",
+    ft = "helm",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.helm_ls.setup({
+        settings = {
+          ["helm-ls"] = {
+            yamlls = {
+              path = "yaml-language-server",
+            },
+          },
+        },
+      })
+    end,
   },
 }
