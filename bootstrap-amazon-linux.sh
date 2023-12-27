09725a3e07c30f2abe8fa68698428c773d7231b0
OkkarMin <omin001@e.ntu.edu.sg>
2023-12-27 14:16:40 +0800

    feat: add z to bootstrap-al2

--- bootstrap-amazon-linux.sh
+++ bootstrap-amazon-linux.sh
@@ -31,6 +31,7 @@ brew install chezmoi
 fish
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
 fisher install PatrickF1/fzf.fish
+fisher install jethrokuan/z
 
 # setup chezmoi
 chezmoi init https://github.com/OkkarMin/dotfiles.git
