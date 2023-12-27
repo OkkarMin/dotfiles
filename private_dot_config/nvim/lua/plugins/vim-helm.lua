d9fed7ad8290a7659d3253d9ea3e317d3cd12bc0
OkkarMin <omin001@e.ntu.edu.sg>
2023-12-27 14:19:38 +0800

    nvim: comment nvim-lspconfig set up

--- private_dot_config/nvim/lua/plugins/vim-helm.lua
+++ private_dot_config/nvim/lua/plugins/vim-helm.lua
@@ -3,20 +3,20 @@ return {
     "towolf/vim-helm",
     ft = "helm",
   },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-
-      lspconfig.helm_ls.setup({
-        settings = {
-          ["helm-ls"] = {
-            yamlls = {
-              path = "yaml-language-server",
-            },
-          },
-        },
-      })
-    end,
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   config = function()
+  --     local lspconfig = require("lspconfig")
+  --
+  --     lspconfig.helm_ls.setup({
+  --       settings = {
+  --         ["helm-ls"] = {
+  --           yamlls = {
+  --             path = "yaml-language-server",
+  --           },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
 }
