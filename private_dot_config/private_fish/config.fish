28284c4b0c6f4d7c6c6b2a24e080ff443e0dd2c2
OkkarMin <omin001@e.ntu.edu.sg>
2024-01-02 21:45:31 +0800

    fish: use zoxide instead of z

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,39 +1,3 @@
-#### Theme
-# # TokyoNight Color Palette
-# set -l foreground c8d3f5
-# set -l selection 2d3f76
-# set -l comment 636da6
-# set -l red ff757f
-# set -l orange ff966c
-# set -l yellow ffc777
-# set -l green c3e88d
-# set -l purple fca7ea
-# set -l cyan 86e1fc
-# set -l pink c099ff
-#
-# # Syntax Highlighting Colors
-# set -g fish_color_normal $foreground
-# set -g fish_color_command $cyan
-# set -g fish_color_keyword $pink
-# set -g fish_color_quote $yellow
-# set -g fish_color_redirection $foreground
-# set -g fish_color_end $orange
-# set -g fish_color_error $red
-# set -g fish_color_param $purple
-# set -g fish_color_comment $comment
-# set -g fish_color_selection --background=$selection
-# set -g fish_color_search_match --background=$selection
-# set -g fish_color_operator $green
-# set -g fish_color_escape $pink
-# set -g fish_color_autosuggestion $comment
-#
-# # Completion Pager Colors
-# set -g fish_pager_color_progress $comment
-# set -g fish_pager_color_prefix $cyan
-# set -g fish_pager_color_completion $foreground
-# set -g fish_pager_color_description $comment
-# set -g fish_pager_color_selected_background --background=$selection
-
 # UTF and Lang
 set -g LANG en_US.UTF-8
 set -g LC_ALL en_US.UTF-8
@@ -47,6 +11,9 @@ end
 # Source starship prompt
 starship init fish | source
 set fish_key_bindings fish_user_key_bindings
+
+# Source zoxide init
+zoxide init fish | source
 
 #### Path to Node ####
 fish_add_path /Users/okar/.local/share/nvm/v18.16.0/bin
