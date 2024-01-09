421958f0e0be9f7dc553762f74ae83dbe65fbcc3
OkkarMin <omin001@e.ntu.edu.sg>
2024-01-09 23:48:54 +0800

    nvim: add nvim-bqf

---
+++ private_dot_config/nvim/lua/plugins/nvim-bqf.lua
@@ -0,0 +1,8 @@
+return {
+  { "kevinhwang91/nvim-bqf", event = "VeryLazy", opts = {} },
+  {
+    "junegunn/fzf",
+    event = "VeryLazy",
+  },
+  { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
+}
