587a975c604eef07bcc01e2a388c1cc06347ceab
OkkarMin <omin001@e.ntu.edu.sg>
2024-01-09 23:51:30 +0800

    nvim: add event = "VeryLazy" to relevant plugins

--- private_dot_config/nvim/lua/plugins/diffview.lua
+++ private_dot_config/nvim/lua/plugins/diffview.lua
@@ -1,6 +1,7 @@
 return {
   {
     "sindrets/diffview.nvim",
+    event = "VeryLazy",
     keys = {
       { "<leader>gv", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
     },
--- private_dot_config/nvim/lua/plugins/spectre.lua
+++ private_dot_config/nvim/lua/plugins/spectre.lua
@@ -1,5 +1,6 @@
 return {
   { "nvim-pack/nvim-spectre", opts = {
+    event = "VeryLazy",
     open_cmd = "noswapfile new",
   } },
 }
