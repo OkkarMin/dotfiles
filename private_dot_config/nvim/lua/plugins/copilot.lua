cb610763b4a1746002fbf8724d0b0feb4be39722
OkkarMin <omin001@e.ntu.edu.sg>
2024-01-16 19:54:36 +0800

    nvim: add copilot

---
+++ private_dot_config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = { enabled = true },
+      panel = { enabled = true },
+      filetypes = {
+        yaml = true,
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+}
